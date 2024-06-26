33e96742adb175cf2ae920f065a2ea2eff868215
Nino <68068169+NinoHuber@users.noreply.github.com>
2024-06-26 13:59:17 +0200

    Add files via upload

---
+++ main.js
@@ -0,0 +1,46 @@
+function slider(direkcija) {
+    let kodovi = document.getElementsByClassName("python")
+    for(let i = 0; i < kodovi.length; i++) {
+        if(kodovi[i].classList.contains("show")) {
+            kodovi[i].classList.remove("show")
+            if(direkcija === "sljedeci") {
+                if(i === kodovi.length-1) {
+                    kodovi[0].classList.add("show")
+                }else {
+                    kodovi[i+1].classList.add("show")
+                }
+            }
+            else {
+                if(i === 0) {
+                    kodovi[kodovi.length-1].classList.add("show")
+                }else {
+                    kodovi[i-1].classList.add("show")
+                }
+            }
+            break
+        }
+    }
+}
+
+function animacija() {
+    anime({
+        targets: '.anim',
+        translateX: 270,
+        opacity: 1,
+        duration: 2000,
+        delay: anime.stagger(200)
+      });
+}
+animacija()
+
+function verifikacija() {
+    let ime = document.getElementById("ime")
+    let komentar = document.getElementById("komentar")
+    if(ime.value.length < 3) {
+        alert("Ime mora imati barem 3 slova")
+    }else if(komentar.value === "") {
+        alert("Komentar mora imati komentar")
+    }else {
+        alert("Uspješno poslano")
+    }
+}
